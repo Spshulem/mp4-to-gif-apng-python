11318daa6d39282647a0dc9f08bbdd9322b2339d
Spencer <spshulem@gmail.com>
2023-06-07 22:18:20 -0400

    Support GIF

---
+++ README.md
@@ -0,0 +1,25 @@
+#MP4 to APNG/GIF Converter
+
+This Python script allows you to convert an MP4 video file into an APNG (Animated Portable Network Graphics) or GIF (Graphics Interchange Format) file. The script prompts the user for the input file path, the desired resolution, the target frame rate, and the output format.
+
+##Requirements
+
+- Python 3.6 or above
+- moviepy (`pip install moviepy`)
+- apng (`pip install apng`)
+
+
+##Usage
+
+1. Ensure you have Python installed on your machine. This script was written in Python 3 and might not work with Python 2.
+2. Install the required Python packages. You can do this by running `pip install moviepy apng` in your command line.
+3. Run the script by navigating to its directory in the command line and running `python mp4_to_apng.py`.
+4. When prompted, enter the path to the input MP4 file, the desired resolution (e.g., 360, 480, 720, 1080), the target frame rate (e.g., 24, 30, 60), and the output format (either 'APNG' or 'GIF').
+
+The script will then convert the video to the chosen format in the specified resolution and frame rate. The output file will be saved in the "export" directory, and any temporary frame files will be deleted after the conversion is complete.
+
+##Notes
+
+- The APNG format is not supported by all platforms and browsers. Ensure that your target platform/browser supports APNG before using this option.
+- The script creates the "export" directory in the same directory where the script is run, not necessarily where the script file is located. If you run the script from a different directory, the "export" directory might be created there instead.
+Please let me know if you need further information or if you would like me to add more details to the README.
--- mp4_to_apng.py
+++ mp4_to_apng.py
@@ -38,15 +38,53 @@ def convert_mp4_to_apng(input_file, output_file, resolution, target_fps, export_
     for png_image in png_images:
         os.remove(png_image)
 
+def convert_mp4_to_gif(input_file, output_file, resolution, target_fps, export_dir):
+    # Create the export directory if it doesn't exist
+    if not os.path.exists(export_dir):
+        os.makedirs(export_dir)
+
+    # Load the MP4 video
+    video = VideoFileClip(input_file)
+
+    # Resize video according to resolution
+    video = video.resize(height=resolution)
+
+    # Reduce frame rate
+    video = video.set_duration(video.duration).set_fps(target_fps)
+
+    # Change extension of output file to .gif
+    output_file_gif = os.path.join(export_dir, output_file.replace('.apng', '.gif'))
+
+    # Write GIF file
+    print(f"Writing GIF file to {output_file_gif}...")
+    video.write_gif(output_file_gif, fps=target_fps)
+    print(f"Finished writing GIF file.")
+
+    if os.path.exists(output_file_gif):
+        print(f"GIF file exists at {output_file_gif}")
+    else:
+        print(f"GIF file does not exist at {output_file_gif}")
+
 if __name__ == "__main__":
     input_file = input("Enter the input MP4 file path: ")
-    file_name, _ = os.path.splitext(input_file)
+    file_name, _ = os.path.splitext(os.path.basename(input_file))
     output_file = f"{file_name}.apng"
 
     resolution = int(input("Enter the resolution (360, 480, 720, 1080 etc.): "))
     target_fps = int(input("Enter the frame rate (24, 30, 60 etc.): "))
-
     export_dir = "export"
-    print(f"Converting {input_file} to {os.path.join(export_dir, output_file.replace('.apng', '.png'))}...")
-    convert_mp4_to_apng(input_file, output_file, resolution, target_fps, export_dir)
+    if not os.path.exists(export_dir):
+        os.makedirs(export_dir)
+    print(f"Export directory: {os.path.abspath(export_dir)}")
+
+    format_choice = input("Enter the output format (APNG or GIF): ").upper()
+    if format_choice == 'APNG':
+        print(f"Converting {input_file} to {os.path.join(export_dir, output_file.replace('.apng', '.png'))}...")
+        convert_mp4_to_apng(input_file, output_file, resolution, target_fps, export_dir)
+    elif format_choice == 'GIF':
+        output_file_gif = f"{file_name}.gif"
+        print(f"Converting {input_file} to {os.path.join(export_dir, output_file_gif)}...")
+        convert_mp4_to_gif(input_file, output_file_gif, resolution, target_fps, export_dir)
+    else:
+        print("Invalid format choice. Please choose either APNG or GIF.")
     print("Conversion complete.")
